1e8f61afba7db5c5c948fe106850bd57c398aeaf
TheCrunchy <crunchycamsta@gmail.com>
2020-11-05 00:42:15 +0000

    added the purger

--- NationsPlugin/Commands.cs
+++ NationsPlugin/Commands.cs
@@ -7,11 +7,14 @@ using Sandbox.ModAPI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Torch.API.Managers;
 using Torch.Commands;
 using Torch.Commands.Permissions;
+using Torch.Managers;
+using VRage.Game;
 using VRage.Game.ModAPI;
 using VRageMath;
 
@@ -35,7 +38,111 @@ namespace NationsPlugin
             }
             return null;
         }
+        [Command("factionpurge", "Purge factions if all members havent logged on for x days")]
+        [Permission(MyPromoteLevel.Admin)]
+        public void purgeFactions(int days, Boolean zero = false)
+        {
+            if (days == 0 && !zero)
+            {
+                Context.Respond("Are you sure? this probably isnt a good idea, attach true to end of command.");
+                return;
+            }
+            int purgedFactions;
+            var cutoff = DateTime.Now - TimeSpan.FromDays(days);
+            List<MyFaction> purging = new List<MyFaction>();
+            //rewrite this shit entirely
+            foreach (KeyValuePair<long, MyFaction> f in MySession.Static.Factions)
+            {
+                bool purged = true;
+                bool npc = false;
+                bool isPurging = true;
+                //bool notPurging = false;
 
+
+                //if (f.Value.Members.Count > 0)
+                //{
+
+                //}
+                //else
+                //{
+                //    purging.Add(f.Value);
+                //    break;
+                //}
+                if (f.Value.Tag.Length > 3)
+                {
+                    //breaking here fucks up everything 
+                    ///break;
+                }
+                else
+                {
+                    foreach (KeyValuePair<long, MyFactionMember> m in f.Value.Members)
+                    {
+                        //do this shit
+
+                        MyIdentity test = MySession.Static.Players.TryGetIdentity(m.Key);
+
+
+                        if (test != null && MySession.Static.Players.IdentityIsNpc(test.IdentityId))
+                        {
+                            purged = false;
+                            npc = true;
+                            if (purging.Contains(f.Value))
+                            {
+                                purging.Remove(f.Value);
+                            }
+
+                        }
+                        if (test != null && test.DisplayName != null && test.LastLoginTime < cutoff && isPurging && !npc)
+                        {
+                            //debug messages
+
+                            purged = true;
+
+                        }
+                        else
+                        {
+                            purged = false;
+                            if (purging.Contains(f.Value))
+                            {
+                                purging.Remove(f.Value);
+                            }
+                     
+                                isPurging = false;
+                          
+
+                        }
+                    }
+                    if (purged)
+                    {
+
+                        purging.Add(f.Value);
+                        List<long> kick = new List<long>();
+                        foreach (KeyValuePair<long, MyFactionMember> m in f.Value.Members)
+                        {
+                            kick.Add(m.Key);
+
+                        }
+                        foreach (long n in kick)
+                        {
+                            f.Value.KickMember(n);
+                        }
+                    }
+
+                }
+            }
+            Context.Respond("Purged " + purging.Count);
+            foreach (MyFaction f in purging)
+            {
+                //add this to a logger
+                NationsPlugin.Log.Info("Purging " + f.Name + "## TAG : " + f.Tag);
+
+                NetworkManager.RaiseStaticEvent(_factionChangeSuccessInfo, MyFactionStateChange.RemoveFaction, f.FactionId, f.FactionId, 0L, 0L);
+                if (!MyAPIGateway.Session.Factions.FactionTagExists(f.Tag)) break;
+                MyAPIGateway.Session.Factions.RemoveFaction(f.FactionId);
+            }
+
+        }
+        private static MethodInfo _factionChangeSuccessInfo = typeof(MyFactionCollection).GetMethod("FactionStateChangeSuccess", BindingFlags.NonPublic | BindingFlags.Static);
 
         [Command("stripalliances", "declare war on everyone")]
         [Permission(MyPromoteLevel.Admin)]
